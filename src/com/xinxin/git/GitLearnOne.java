764858a7ed19b912a6b7b664e70461d58001d01a
grape <179831336@qq.com>
2019-05-21 11:13:45 +0800

    通过eclipse工具提交

--- src/com/xinxin/git/GitLearnOne.java
+++ src/com/xinxin/git/GitLearnOne.java
@@ -2,6 +2,6 @@ package com.xinxin.git;
 
 public class GitLearnOne {
 	public static void main(String[] args) {
-		System.out.println("学习git管理项目");
+		System.out.println("学习git管理项目，引入到eclipse");
 	}
 }
