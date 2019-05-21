3d270d4f0353312246ea9d06c7633f666615cc4b
grape <179831336@qq.com>
2019-05-21 11:26:24 +0800

    为提交点打了tag

--- src/com/xinxin/git/GitLearnOne.java
+++ src/com/xinxin/git/GitLearnOne.java
@@ -2,6 +2,6 @@ package com.xinxin.git;
 
 public class GitLearnOne {
 	public static void main(String[] args) {
-		System.out.println("学习git管理项目，引入到eclipse,当前为DEV分支");
+		System.out.println("学习git管理项目，引入到eclipse,当前为DEV分支，已经打过了tag");
 	}
 }
